7749c123c42afbfa127b315a841da241da354706
MichitakaTsuda <67595708+MichitakaTsuda@users.noreply.github.com>
2022-07-25 11:53:03 +0900

    memo

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,17 +1,20 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
+//memo()で囲うことで、Propsが変更されない限り、親のコンポーネントが再レンダリングされても再レンダリング（影響）されない
+export const ChildArea = memo((props) => {
   const { open } = props;
   console.log("ChildAreaがレンダリングされた");
 
   const data = [...Array(2000).keys()];
-  data.forEach(() => {
-    console.log("...");
-  });
+  // data.forEach(() => {
+  //   console.log("...");
+  // });
 
   console.log(data);
   return (
@@ -23,4 +26,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
